3c5c8329ae4a7d10c709344966e9e9462149fbf0
ainochi-kor <kthrkdals@gmail.com>
2024-11-23 16:11:36 +0900

    feat: chat form

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,21 +1,35 @@
 "use client";
 
+import BottomBar from "@/components/BottomBar";
 import SideBar from "@/components/SideBar";
+import { auth } from "@/firebase";
+import { User } from "firebase/auth";
 import { useParams } from "next/navigation";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const ChatPage: React.FC = () => {
   const { id } = useParams<{
     id: string;
   }>();
+  const [userMe] = useAuthState(auth);
 
   return (
     <main className="grid w-full grid-cols-8">
       <div className="col-span-2">
         <SideBar selectedChatId={id} />
       </div>
-      <div className="col-span-6 flex justify-center h-screen">
-        <div className="flex flex-col items-center justify-center space-y-4"></div>
+      <div className="flex flex-col w-full col-span-6">
+        {/* Top bar  */}
+        Top bar
+        <div className="flex w-full h-full px-6 pt-4 mb-2 overflow-y-scroll no-scrollbar">
+          <div className="flex flex-col w-full h-full ">
+            {/* Messages */}
+            Messages
+          </div>
+        </div>
+        {/* Bottom bar */}
+        <BottomBar user={userMe as User} chatId={id} />
       </div>
     </main>
   );
--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,11 +1,54 @@
 "use client";
 
-import React from 'react'
+import { db } from "@/firebase";
+import { User } from "firebase/auth";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import React, { startTransition, useState } from "react";
+import { IoPaperPlaneOutline } from "react-icons/io5";
 
-const BottomBar = () => {
-  return (
-    <div>BottomBar</div>
-  )
+interface BottomBarProps {
+  user: User;
+  chatId: string;
 }
 
-export default BottomBar
+const BottomBar: React.FC<BottomBarProps> = ({
+  chatId,
+  user,
+}: BottomBarProps) => {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (input.trim().length === 0) return;
+    const text = input.trim();
+    await addDoc(collection(db, `chats/${chatId}/messages`), {
+      text,
+      sender: user.email,
+      photoURL: user.photoURL,
+      timestamp: serverTimestamp(),
+    });
+  };
+
+  return (
+    <div>
+      <form
+        className="flex items-end w-full px-6 pb-4 space-x-2"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="메시지를 입력하세요."
+          className="w-full px-4 py-4 placeholder-gray-400 border border-gray-200 rounded-lg focus:border-gray-400"
+          value={input}
+          onChange={(e) => startTransition(() => setInput(e.target.value))}
+        />
+        <button>
+          <IoPaperPlaneOutline className="mb-4 text-gray-600 w-7 h-7 hover:text-gray-900" />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default BottomBar;
