905e3705f63f479a8eadbc012c014ffe28b05ed4
ainochi-kor <kthrkdals@gmail.com>
2024-11-23 16:11:52 +0900

    feat: load chat message

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -2,16 +2,32 @@
 
 import BottomBar from "@/components/BottomBar";
 import SideBar from "@/components/SideBar";
-import { auth } from "@/firebase";
+import { auth, db } from "@/firebase";
 import { User } from "firebase/auth";
+import { collection, doc, orderBy, query } from "firebase/firestore";
 import { useParams } from "next/navigation";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  useCollectionData,
+  useDocumentData,
+} from "react-firebase-hooks/firestore";
+import { CgSpinner } from "react-icons/cg";
+import { IoChatbubblesOutline } from "react-icons/io5";
 
 const ChatPage: React.FC = () => {
   const { id } = useParams<{
     id: string;
   }>();
+
+  const chatQuery = query(
+    collection(db, "chats", id, "messages"),
+    orderBy("timestamp")
+  );
+
+  const [messages, loading] = useCollectionData(chatQuery);
+  const [chat] = useDocumentData(doc(db, "chats", id));
+
   const [userMe] = useAuthState(auth);
 
   return (
@@ -25,7 +41,19 @@ const ChatPage: React.FC = () => {
         <div className="flex w-full h-full px-6 pt-4 mb-2 overflow-y-scroll no-scrollbar">
           <div className="flex flex-col w-full h-full ">
             {/* Messages */}
-            Messages
+            {loading && (
+              <div className="flex flex-col w-full h-full">
+                <CgSpinner className="animate-spin w-12 h-12 text-gray-400" />
+              </div>
+            )}
+            {!loading && messages?.length === 0 && (
+              <div className="flex flex-col items-center justify-center flex-1">
+                <IoChatbubblesOutline className="w-24 h-24 text-gray-300" />
+                <p className="mt-2 text-2xl font-medium tracking-tight text-gray-300 ">
+                  대화를 시작합니다.
+                </p>
+              </div>
+            )}
           </div>
         </div>
         {/* Bottom bar */}
